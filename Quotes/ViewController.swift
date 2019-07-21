069a7dd668c776bf3db0081744574ac8d870a9f3
Kharnyee <kharnyee@icloud.com>
2019-07-21 20:50:21 +0800

    Initial Commit

---
+++ Quotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quotes
+//
+//  Created by Kharnyee Eu on 21/07/2019.
+//  Copyright © 2019 focus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
