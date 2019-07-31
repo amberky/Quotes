6452a8144520c5fef13cf501514381022bd3091a
Kharnyee <kharnyee@icloud.com>
2019-07-31 10:57:02 +0800

    Add SmallIconCollectionViewCell back to directory

---
+++ Quotes/Views/Custom Views/SmallIconCollectionViewCell/SmallIconCollectionViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  SmallIconCollectionViewCell.swift
+//  Quotes
+//
+//  Created by Kharnyee Eu on 25/07/2019.
+//  Copyright © 2019 focus. All rights reserved.
+//
+
+import UIKit
+
+class SmallIconCollectionViewCell: UICollectionViewCell {
+
+//    @IBOutlet var viewCell: SmallIconCollectionViewCell!
+    
+    @IBOutlet weak var iconBackground: UIView!
+    @IBOutlet weak var iconImage: UIImageView!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+    
+}
